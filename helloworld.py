a346bed5a61b90e9df38f1457104fff780cb803b
sunjiyun26 <sunjiyun26@126.com>
2016-05-30 16:51:40 +0800

    在文件头增加 /usr/bin/env 让Linux系统识别能执行这个文件

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 print 'helloworld'
 print 'sunjiyun'
 print 'sunjiyun2'
--- step2.py
+++ step2.py
@@ -1,2 +1,4 @@
+#!/usr/bin/env python
 print 'this is test python convert , to space \' yes1\',\'yes2\':  ',' yes1','yes2'
 name = raw_input()
+print name
