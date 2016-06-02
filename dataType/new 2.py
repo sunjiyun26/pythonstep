13a7a6d0f258e2352da2ac1293b5e357e44c7406
sunjiyun26 <sunjiyun26@126.com>
2016-06-02 08:54:16 +0800

    数据类型，在windows下直接用notepad创建的文件在cmd窗口下运行会出现乱码，用新建txt然后改为.py的不会出现乱码

---
+++ dataType/new 2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*- 
+print '在python中只有 True 和False 两种类型 其他的任何 大小写不符合的 如 true false  TRUE FALSE都会编译报错'
+print True
+print False
+print 'print TRUE 会报错'
+print 'print FALSE 会报错'
+print 'print true 会报错'
+print 'print false 会报错'
