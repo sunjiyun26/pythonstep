a83ad852f634c7432280f6174418c9bebf89814d
sunjiyun26 <sunjiyun26@126.com>
2016-06-07 20:18:59 +0800

    xiugaiset.py 文件

--- dictset/set.py
+++ dictset/set.py
@@ -11,3 +11,10 @@ print myset
 print 'set delete element with remove function myset.remove(\'sunjiyun\')'
 myset.remove("sunjiyun")
 print myset
+
+print ">"*20
+print "取交集 set 的方法是 用 & 符号  s1 * s2,set 保留相同的值"
+myset1 = set([1,2,3,4,5,6])
+myset2 = set([4,5,6,7,8,9])
+
+print myset1 & myset2
