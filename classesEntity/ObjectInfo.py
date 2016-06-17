aa08064a4b6f937cfce2f0fd40564d55be2b3a6d
sunjiyun26 <sunjiyun26@126.com>
2016-06-17 13:41:39 +0800

    这是通过pycharm 查看类的属性 setattr getattr

--- classesEntity/ObjectInfo.py
+++ classesEntity/ObjectInfo.py
@@ -11,8 +11,31 @@ print type(abs)
 
 print type(123) == types.IntType
 print type("123") == types.IntType
-print type("123") == types.StringType,"\n"
+print type("123") == types.StringType, "\n"
 print type(int) == type(str) == types.TypeType
 
 print dir(str)
 
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        pass
+
+    def __len__(self):
+        return 99
+
+
+if __name__ == '__main__':
+    student = Student('sunjiyun', {'age': 31})
+    print len(student)
+    print u'判断是否含有attr hasattr'
+    print hasattr(student, "name")
+    print u'set attribute with setattr '
+    print 'hasattr(student,"score")', hasattr(student, 'score')
+    setattr(student, 'score','80')
+    print 'after setattr hasattr(student,"score")', hasattr(student, 'score')
+    print getattr(student,'score')
+    print 'do you know what mean below'
+    print getattr(student,'address','jinan')
