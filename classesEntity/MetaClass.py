43ed071ee121f2f5486d3d9ef041bf21f3a3578a
sunjiyun26 <sunjiyun26@126.com>
2016-06-22 09:38:27 +0800

    这是通过pycharm 补齐多重继承的函数，元类

---
+++ classesEntity/MetaClass.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'sunjiyun'
+
+def helloWorld(self, name="world"):
+    print 'hello ,%s' % name
+if __name__ == "__main__":
+    HelloWorld = type('HELLO2', (object,),dict(helloWorld2 = helloWorld))
+    hello2 =  HelloWorld()
+    # hello2.
--- classesEntity/multipleinheritance.py
+++ classesEntity/multipleinheritance.py
@@ -1,5 +1,41 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 __author__ = 'sunjiyun'
+from SocketServer import TCPServer,UDPServer
 print '多重继承'
+class Animal:
+    pass
+class Bird(Animal):
+    pass
+class Mammal(Animal):
+    pass
+
+class Runnale(object):
+    def run(self):
+        print 'running'
+class Flyable(object):
+    def fly(self):
+        print 'flyable'
+class Dog(Mammal,Runnale):
+    def __init__(self):
+        print 'i am a dog,i am Mammal i can'
+class Parrot(Bird,Flyable):
+    def __init__(self):
+        print("这是鹦鹉,",self.fly())
+
+if __name__ == "__main__":
+    dog = Dog()
+    dog.run()
+    parrot = Parrot()
+    parrot.fly()
+
+
+class MyTCPServer(TCPServer):
+    def __init__(self):
+        print 'this is tcpSever'
+class MyUDPServer(UDPServer):
+    def __init__(self):
+        print 'this is UDPSERVER'
+if __name__ =="__main__":
+    myTcpServer = MyTCPServer()
 
