959405536411370064b976e3933192da28fa965f
sunjiyun26 <sunjiyun26@126.com>
2016-06-22 17:53:44 +0800

    修改元类代码 提价

--- classesEntity/MetaClass.py
+++ classesEntity/MetaClass.py
@@ -8,3 +8,8 @@ if __name__ == "__main__":
     HelloWorld = type('HELLO2', (object,),dict(helloWorld2 = helloWorld))
     hello2 =  HelloWorld()
     # hello2.
+class ListMetaclass(type):
+    print "metaclass is from type"
+    def __new__(cls, *args, **kwargs):
+        pass
+
