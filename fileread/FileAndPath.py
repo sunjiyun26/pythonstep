49ffa1085a7d95f1cf3b42ac6aca06c94c722cbe
sunjiyun26 <sunjiyun26@126.com>
2016-06-28 10:08:40 +0800

    文件操作 os.path 提供了一系列的工具类方法

--- fileread/FileAndPath.py
+++ fileread/FileAndPath.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 __author__ = 'sunjiyun'
 import os
+
 print os.name
 # print os.uname()  windows 不提供 linux 等提供
 print os.environ
@@ -9,6 +10,9 @@ print os.getenv("path")
 print os.getenv("JAVA_HOME")
 print os.path.realpath
 print os.path.abspath(".")
-print  os.path.join("e:\\","sunjiyun")
-# os.mkdir("e:\\sunjiyun")
+print  os.path.join("e:\\", "sunjiyun")
+os.mkdir("e:\\sunjiyun")
 os.rmdir("e:\\sunjiyun")
+
+print os.listdir("E:\\")
+print [x for x in os.listdir("E:\\") if os.isdir(x) ]
